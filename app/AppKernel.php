1d280ebe06b56287fdc3d5e8bee942de16480693
Jérôme poskin <jerome@snowcap.be>
2013-04-22 23:15:56 +0200

    Creating basic slider

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Snowcap\CoreBundle\SnowcapCoreBundle(),
             new Snowcap\ImBundle\SnowcapImBundle(),
+            new Photolorent\SiteBundle\PhotolorentSiteBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
