a5cf002c97d76551731154b5b848c438b98201e2
Jérôme Poskin <jerome@snowcap.be>
2014-05-20 10:20:08 +0200

    Updating to Symfony 2.4.4

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,9 +10,8 @@ use Symfony\Component\Debug\Debug;
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE))
-    || !filter_var(@$_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP)
-    || (filter_var(@$_SERVER['REMOTE_ADDR'], FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE) && !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1')))
+    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
+    || !in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', 'fe80::1', '::1'))
 ) {
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
